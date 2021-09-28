c1e39675fc7c79706c875bc3142342afc01cde62
fcataniae <cataniafrane@gmail.com>
2021-09-27 23:55:09 -0300

    - add mutants endpoint - add service and handlers for logic - add dependencies and sonar properties - add sonar integration - add test

---
+++ src/main/java/com/meli/codechallenge/handler/MutantHandler.java
@@ -0,0 +1,26 @@
+package com.meli.codechallenge.handler;
+
+import com.meli.codechallenge.dto.Dna;
+import com.meli.codechallenge.service.MutantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MutantHandler {
+
+    private final MutantService mutantService;
+
+    @Autowired
+    public MutantHandler(MutantService mutantService) {
+        this.mutantService = mutantService;
+    }
+
+    public Mono<ServerResponse> isMutant(ServerRequest serverRequest) {
+
+        return serverRequest.bodyToMono(Dna.class)
+                .flatMap(mutantService::applyMutantValidation);
+    }
+}
--- src/main/java/com/meli/codechallenge/router/Router.java
+++ src/main/java/com/meli/codechallenge/router/Router.java
@@ -1,8 +1,10 @@
 package com.meli.codechallenge.router;
 
 import com.meli.codechallenge.handler.HealthHandler;
+import com.meli.codechallenge.handler.MutantHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -10,13 +12,14 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration(proxyBeanMethods = false)
-public class HealthRouter {
+public class Router {
 
     @Bean
-    public RouterFunction<ServerResponse> route(HealthHandler helloHandler) {
+    public RouterFunction<ServerResponse> route(HealthHandler helloHandler, MutantHandler mutantHandler) {
 
         return RouterFunctions
-                .route(GET("/_ah/health"), helloHandler::hello);
+                .route(GET("/_ah/health"), helloHandler::hello)
+                .andRoute(POST("/mutant").and(accept(MediaType.APPLICATION_JSON)), mutantHandler::isMutant);
     }
 
 }
